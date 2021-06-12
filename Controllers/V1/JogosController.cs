b52cfe8290b52842c5c8bfabf2317204b48279ec
Alex Francys <francys_alex2007@hotmail.com>
2021-06-12 18:44:08 -0300

    Criando Models

--- Controllers/V1/JogosController.cs
+++ Controllers/V1/JogosController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ApiCatalogoJogosAlex.InputModel;
+using ApiCatalogoJogosAlex.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,25 +14,25 @@ namespace ApiCatalogoJogosAlex.Controllers.V1
     public class JogosController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<object>>> Obter()
+        public async Task<ActionResult<List<JogoViewModel>>> Obter()
         {
             return Ok();
         }
 
-        [HttpGet("{idJogo:guid}")]
-        public async Task<ActionResult<object>> Obter(Guid idJogo)
+        [HttpGet("{idJogo:guid}")] //obter um unico jogo
+        public async Task<ActionResult<JogoViewModel>> Obter(Guid idJogo)
         {
             return Ok();
         }
 
         [HttpPost]
-        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        public async Task<ActionResult<JogoViewModel>> InserirJogo(JogoInputModel jogo)
         {
             return Ok();
         }
 
         [HttpPut]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, Object jogo)
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputModel jogo)
         {
             return Ok();
         }
